bd715d665053f9e732ed3dd500dc65031f1058cf
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-10-16 21:07:21 +0100

    Update app.json splash image, add NotFound component, and create new index.jsx file. Rename _layout.tsx to _layout.jsx and remove old index.tsx. Update tsconfig to include JavaScript files. Add logo image.

---
+++ app/+not-found.jsx
@@ -0,0 +1,22 @@
+import { View, Image } from "react-native";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+      }}
+    >
+      <Image
+        source={require("@/assets/images/logo.png")}
+        style={{ width: "100%", height: 150 }}
+      />
+    </View>
+  );
+};
+
+export default NotFound;
