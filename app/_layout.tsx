4b150e9f550eca94b17a53d4072d61546ea182c2
lima8002 <57651553+lima8002@users.noreply.github.com>
2024-12-10 00:10:54 +0000

    Update splash screen image and implement user authentication logic in layout. Adjust navigation based on authentication status.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,34 @@
-import { useEffect } from "react";
-import { SplashScreen, Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { router, SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
+import { AuthenticatedUser } from "@/utils/FirebaseHelper";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    boolean | null
+  >(null);
+
   const [fontsLoaded, error] = useFonts({
     outfit: require("../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
   });
+
+  useEffect(() => {
+    const unsubscribe = AuthenticatedUser((user) => {
+      if (user) {
+        setIsUserAuthenticated(true);
+        router.replace("/home"); // Redirect to home page if user is authenticated
+      } else {
+        setIsUserAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   useEffect(() => {
     if (error) throw error;
@@ -24,18 +42,19 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
   return (
     <>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(meds)" options={{ headerShown: false }} />
-      </Stack>
+      {isUserAuthenticated ? (
+        <Stack screenOptions={{ headerShown: false }} initialRouteName="home">
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(meds)" options={{ headerShown: false }} />
+        </Stack>
+      ) : (
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      )}
       <StatusBar style="dark" />
     </>
   );
